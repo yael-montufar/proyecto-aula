b7695be8af49aa7973b5f3e22a02563fb02b3ee2
Yael Montufar <ym@Yaels-MacBook-Pro.local>
2023-05-03 02:30:02 -0600

    Configure navigation through user authentication

--- App.js
+++ App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import './firebaseConfig';
+import RootNavigation from './app/navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Nuestro Proyecto</Text>
-      <StatusBar style="auto" />
-    </View>
+    <RootNavigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
